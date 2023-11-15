fd0762bd6e19a85ea5c68d79f8d98b6fc514ac2a
Dmitriy Makuhin <typedefinc@gmail.com>
2023-11-15 19:38:04 +0300

    feature(develop): add docker configuration

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export const app = buildApp(options);
 const port: number = (process.env.APP_PORT || 3000) as number;
 
 try {
-  app.listen({ port });
+  app.listen({ port, host: '0.0.0.0' });
 } catch (error: unknown) {
   app.log.error(error);
   process.exit(1);
